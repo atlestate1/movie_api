afb2a4153ba0434ce08fcb9c7ab6b990349ff7bb
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2022-02-25 13:03:08 -0500

    code updates
    updated code to connect heroku and atlas

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const { check, validationResult } = require('express-validator');
 const movieAPI = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/movieAPI', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/movieAPI', { useNewUrlParser: true, useUnifiedTopology: true }); //
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const bodyParser = require('body-parser');
 
